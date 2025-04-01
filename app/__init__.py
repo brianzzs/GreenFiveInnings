1b8c4516799a5088f6d5908367617ea194a98779
brianzzs <brianzanoni@outlook.com>
2025-04-01 19:52:35 -0300

    fix - cors

--- app/__init__.py
+++ app/__init__.py
@@ -30,11 +30,9 @@ def create_app(config_name='default'):
     )
     
     CORS(app, resources={r"/*": {"origins": [
-        "https://fiveinnings-api-f6d1667b55fe.herokuapp.com/", 
-        "http://localhost:3000",            
+        "https://fiveinnings.com",
+        "http://localhost:3000",
         "http://localhost:5173",
-        "https://fiveinnings.com/"
-
     ]}})
     
     @app.before_request
